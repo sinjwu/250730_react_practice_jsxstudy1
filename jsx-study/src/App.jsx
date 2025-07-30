e8323c44204a4cc58676eeeeb99ee420ce0bd1b2
sinjwu <harbirdouk@gmail.com>
2025-07-30 11:25:24 +0900

    250730fc1

--- jsx-study/src/App.jsx
+++ jsx-study/src/App.jsx
@@ -1,11 +1,42 @@
 import "./App.css";
 import UserCard from "./components/UserCard";
 
+const userData = [
+  {
+    name: "sinjwu",
+    job: "Programmer",
+    imageSrc: "avatar.webp",
+  },
+  {
+    name: "신정욱",
+    job: "자택경비원",
+    imageSrc: "avatar2.webp",
+  },
+  {
+    name: "월문",
+    job: "반수생",
+    imageSrc: "avatar3.jpeg",
+  },
+];
+
 const App = () => {
   return (
     <>
-      <UserCard name="sinjwu" job="Programmer" imageSrc="avatar.webp" />
-      <UserCard name="신정욱" job="자택경비원" imageSrc="avatar2.webp" />
+      <UserCard
+        name={userData[0].name}
+        job={userData[0].job}
+        imageSrcame={userData[0].imageSrc}
+      />
+      <UserCard
+        name={userData[1].name}
+        job={userData[1].job}
+        imageSrcame={userData[1].imageSrc}
+      />
+      <UserCard
+        name={userData[2].name}
+        job={userData[2].job}
+        imageSrcame={userData[2].imageSrc}
+      />
     </>
   );
 };
